7aa0e3a2872ab82b7cb2f1c494348c1a8a7ec183
codlocker <ipsit.sahoo11@gmail.com>
2017-02-17 16:41:32 +0530

    Implementation of Doxygen here
    Signed-off-by: codlocker <ipsit.sahoo11@gmail.com>

--- playfair.cpp
+++ playfair.cpp
@@ -3,9 +3,13 @@
 #define pb push_back
 #define mp make_pair
 using namespace std;
-string implmatrix(string s, char mat[][5]) {
+/********************************************//**
+ *  The matrix obtained in the main function is now used here to generate the cipher text. According to playfair
+ *  algorithm the conditions for swap vary according to the locations of the digrams in the supplied matrix.
+ ***********************************************/
+string implmatrix(string s /**< String to be encrypted */, char mat[][5]) {
     pair<int,int> p[2];
-    int i,j,k;
+    int i,j,k; //!< Iteration Variables for the matrix
     for(k=0;k<2;k++) {
         for(i=0;i<5;i++)
             for(j=0;j<5;j++)
@@ -14,14 +18,29 @@ string implmatrix(string s, char mat[][5]) {
                 }
     }
     /*Adhering the conditions for swapping values*/
+    ///  First case handling
+     /**
+     *  If the the digrams are not located in the same column and same row then we swap the take the digit in the same row
+     *  but column number of the second member of the diagram and vice versa for the second element of the digram
+     */
     if(p[0].first != p[1].first && p[0].second != p[1].second) {
         s[0] = mat[p[0].first][p[1].second];
         s[1] = mat[p[1].first][p[0].second];
     }
+    ///  Second case handling
+     /**
+     *  If the the digrams are located in the same row and different column then we swap the take the digit in the same row
+     *  but next column number in that row and same is done for the second element of the digram.
+     */
     else if(p[0].first == p[1].first) {
         s[0] = mat[p[0].first][(p[0].second + 1) % 5];
         s[1] = mat[p[1].first][(p[1].second + 1) % 5];
     }
+    ///  Third case handling
+     /**
+     *  If the the digrams are located in the same colum and different row then we swap the take the digit in the same column
+     *  but next row number in that row and same is done for the second element of the digram.
+     */
     else if(p[0].second == p[1].second) {
         s[0] = mat[(p[0].first + 1) % 5][p[0].second];
         s[1] = mat[(p[1].first + 1) % 5][p[1].second];
